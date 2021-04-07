37b311eff7073442807e741c529bec292d46b56f
Natan Souza <partisansouza@gmail.com>
2021-04-06 21:03:57 -0300

    feat(hello.go): add io to read from a file.

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -55,23 +58,48 @@ func exibeMenu() {
 
 func iniciarMonitoramento() {
 	fmt.Println("Monitorando...")
-	sites := []string{
-		"https://www.alura.com.br",
-		"https://www.google.com.br",
-		"https://httpstat.us/422",
-	}
+
+	sites := leSitesDoArquivo()
 
 	for _, site := range sites {
 		testaSite(site)
-		time.Sleep(5 * time.Second)
+		time.Sleep(2 * time.Second)
 	}
 }
 
 func testaSite(site string) {
-	resp, _ := http.Get(site)
-	if resp.StatusCode > 199 && resp.StatusCode < 300 {
-		fmt.Println("Site:", site, "status:", resp.StatusCode)
-	} else {
-		fmt.Println("Site:", site, "status:", resp.StatusCode)
+	resp, err := http.Get(site)
+
+	if err != nil {
+		fmt.Println("Error:", err)
 	}
+
+	if resp.StatusCode > 199 && resp.StatusCode < 300 {
+		fmt.Println("\n|", site, "\n -StatusCode:", resp.StatusCode, "\n -Status: success")
+	} else {
+		fmt.Println("\n|", site, "\n -StatusCode:", resp.StatusCode, "\n -Status: error")
+	}
+}
+
+func leSitesDoArquivo() []string {
+
+	var sites []string
+
+	arquivo, err := os.Open("sites.txt")
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	leitor := bufio.NewReader(arquivo)
+
+	for {
+		linha, err := leitor.ReadString('\n')
+		linha = strings.TrimSpace(linha)
+		sites = append(sites, linha)
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return sites
 }
